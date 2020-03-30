a2ea73b61c1b6898cb3635024171e61b6ec16ff3
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-03-30 18:29:37 +0530

    refactor: Convert class to functional component

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import constants from "../constant";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>{constants.APPLICATION_TITLE}</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>{constants.APPLICATION_TITLE}</h1>
+    </div>
+  );
 }
 
 export default App;
