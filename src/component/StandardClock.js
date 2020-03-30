bec72d144364ed567dd7c0e464bd9acb97f8672b
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-03-30 22:26:40 +0530

    refactor: Convert StandardClock as arrow function

--- src/component/StandardClock.js
+++ src/component/StandardClock.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function StandardClock(props) {
+const StandardClock = props => {
   return <div>{props.time}</div>;
-}
+};
 
 StandardClock.propTypes = {
   time: PropTypes.string.isRequired
